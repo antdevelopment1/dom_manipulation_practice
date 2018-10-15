f2a6bf3593212b526956ed71785e3bdaaa72d469
April Copes <antdevelopment1@gmail.com>
2018-10-15 19:45:35 -0400

    Initial set up for Dom manipulation html structure, javascript comment outline

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+// What is the DOM
+// DOM is the interface between your JavaScript and Html and Css
+// console.dir(document) in the tools list all the properties inside the document
+// Document object model is the root node where everything lives inside of\
+
+
+// 5 Main Methods Covered In This Exercise
+// document.getElementById()
+// document.getElementByClassName()
+// document.getElementByTagName()
+// document.querySelector()
+// document.querySelectorAll()
