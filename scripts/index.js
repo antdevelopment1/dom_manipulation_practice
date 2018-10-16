05a912131d2aecf8c5e2f696aec256c0ceb899d5
April Copes <antdevelopment1@gmail.com>
2018-10-15 20:43:17 -0400

    Example of selecting an element using query selectors

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,7 @@
 // ===============================================
 //         Older Ways To Select Elements
 // ===============================================
+
 // Gets the list item by targeting the id highlight
 var tag = document.getElementById("highlight");
 
@@ -46,4 +47,19 @@ console.dir(tagName);
 //         Newer Ways To Select Elements
 // ===============================================
 
- 
+// In this instance we only get the first match even though we have more than one element with the same class bolded
+// Query Selector can also retrieve a tag name and it also only returns the very first one
+var newerTag = document.querySelector('.bolded');
+console.log(newerTag);
+
+var h1Tag = document.querySelector('h1');
+console.log(h1Tag);
+
+var liTag = document.querySelector('#highlight');
+console.log(liTag);
+
+
+// All anchor tags that have a class special that are nested inside of an li is totally valid
+// This line of code doesn't exsist in our html to be referenced anywhere but make a mental note that this is valid
+// This will return null
+// var li = document.querySelector("li a.special")
