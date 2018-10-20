ee088f13e87de67ccd39453f4dd9712c675feb1b
April Copes <antdevelopment1@gmail.com>
2018-10-20 18:14:04 -0400

    Changes background color when button is clicked

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,31 @@
-var p = document.querySelector("p")
-p.classList.add('big');
-p.classList.remove('big');
-p.classList.toggle('big');
+// var button = document.querySelector('[data-button]');
+// var isPurple = false;
+// button.addEventListener('click', function() {
+//     if (isPurple) {
+//         document.body.style.background = 'white';
+//         isPurple = false;
+//     } else {
+//         document.body.style.background = 'purple';
+//         isPurple = true;
+//     } 
+// })
 
-// 
+// or
+
+// var button = document.querySelector('[data-button]');
+// var isPurple = false;
+// button.addEventListener('click', function() {
+//     if (isPurple) {
+//         document.body.style.background = 'white';
+//     } else {
+//         document.body.style.background = 'purple';
+//     } 
+//     isPurple = !isPurple;
+// })
+
+// or
+
+var button = document.querySelector('[data-button]');
+button.addEventListener('click', function() {
+    document.body.classList.toggle("container"); 
+})
